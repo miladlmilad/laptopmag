0ea7052da30d26a61efc8fca54db62d41999565e
miladlmilad <64641189+miladlmilad@users.noreply.github.com>
2020-05-01 16:23:32 +0430

    Mod added

--- index.php
+++ index.php
@@ -5,5 +5,13 @@ echo "hello";
  $n=0;
   
 while(n<=9){
-           echo"$n"
+           echo $n
            $n++;}
+
+            
+function mod($a){
+            if(a%2==0)
+            echo"e";
+            }else{
+            echo"o";
+            }
